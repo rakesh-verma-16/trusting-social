6aa7ad90050a80690796f226555eb73b2983bbdc
Rakesh Verma <rakesh.verma@pharmeasy.in>
2019-02-20 16:34:04 +0530

    Abstraction implemented - terminal would call init.rb

--- main.rb
+++ main.rb
@@ -4,14 +4,13 @@ require_relative 'utilities/SheetReader'
 # Main method to process a CSV of numbers to return last user's activation date.
 # Params:
 # +sheet_path+:: Path to the sheet.
-# +batch_size+:: Batch size to process limited number of rows from CSV at a time.
-def main(sheet_path = "test-dataset/small-miscellaneous-data.csv", batch_size = 10000)
+# +batch_size+:: Batch size to proces
+def main(sheet_path, batch_size)
 	response = create_timelines_from_numbers(sheet_path, batch_size)
 	SheetReader.write_to_file(response, File.basename(sheet_path, ".csv"))
 end
 
 private
-
 def create_timelines_from_numbers(sheet_path, batch_size)
 	ActivePhone.create_table
 
@@ -19,3 +18,5 @@ def create_timelines_from_numbers(sheet_path, batch_size)
 
 	ActivePhone.find_last_users_activation_date
 end
+
+
